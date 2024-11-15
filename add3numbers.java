5599f798f7e92027cb681ecc3823796345c4f4fc
Dibyajyoti <126963458+DibyajyotiBiswal57@users.noreply.github.com>
2024-11-15 19:32:59 +0530

    Update add3numbers.java

--- add3numbers.java
+++ add3numbers.java
@@ -5,23 +5,21 @@ public class add3numbers {
         // Create a Scanner object to read input from the standard input stream (keyboard)
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user to enter their name
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine(); // Read a line of text
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt(); 
 
-        // Prompt the user to enter their age
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt(); // Read an integer
 
-        // Prompt the user to enter their height
-        System.out.print("Enter your height: ");
-        double height = scanner.nextDouble(); // Read a double
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt(); 
+
+
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt(); 
+
+        int sum = a+b+c
 
         // Display the entered information
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Height: " + height);
-
+        System.out.println("Sum: " +sum);
         // Close the scanner to prevent resource leaks
         scanner.close();
     }
