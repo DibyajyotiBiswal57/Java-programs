95e90bcec5e2f53370150fbbd277adecdf3a3e44
Dibyajyoti <126963458+DibyajyotiBiswal57@users.noreply.github.com>
2024-11-15 19:36:36 +0530

    Update add3numbers.java

--- add3numbers.java
+++ add3numbers.java
@@ -2,25 +2,23 @@ import java.util.Scanner;
 
 public class add3numbers {
     public static void main(String[] args) {
-        // Create a Scanner object to read input from the standard input stream (keyboard)
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter first number: ");
         int a = scanner.nextInt(); 
 
-
         System.out.print("Enter second number: ");
         int b = scanner.nextInt(); 
-
 
         System.out.print("Enter third number: ");
         int c = scanner.nextInt(); 
 
         int sum = a+b+c;
 
-        // Display the entered information
+
         System.out.println("Sum: " +sum);
-        // Close the scanner to prevent resource leaks
+        
         scanner.close();
     }
 }
