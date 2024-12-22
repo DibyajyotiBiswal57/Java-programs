ec8552623adcbfc3eae643a2841fb20e7b816200
Dibyajyoti <dibbispersonal@gmail.com>
2024-12-22 13:43:28 +0530

    Update positiveornegative.java

--- positiveornegative.java
+++ positiveornegative.java
@@ -3,6 +3,7 @@ public class positiveornegative
 {
     public static void main(String args[])
     {
+        System.out.println("This program checks whether a number is positive or negative or 0."); 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number:");
         double a = scanner.nextDouble();
