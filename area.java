17a268391d6ddde94788e040e96fc7f9ebc57da8
Dibyajyoti <dibbispersonal@gmail.com>
2024-12-22 13:38:47 +0530

    Update area.java

--- area.java
+++ area.java
@@ -4,8 +4,18 @@ public class area
 {
     public static void main(String args[])
     {
-        
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("This program finds out the area of a square, rectangle or circle");
+        System.out.println("Choose the shape");
+        System.out.println("For square, type '1'");
+        System.out.println("For rectangle, type '2'");
+        System.out.println("For circle, type '3'");
+        System.out.println("Enter your choice:");
+        int choice = scanner.nextInt();
+
+        if(choice==1)
+        {
 
         System.out.print("Enter the side of the square: ");
         double side = scanner.nextInt();
@@ -13,8 +23,10 @@ public class area
         
         double squarearea= side*side;
         System.out.println("The area of the square is " +squarearea);
+        }
         
-        
+        else if(choice==2)
+        {
         System.out.print("Enter the length of the rectangle: ");
         double len = scanner.nextInt();
         System.out.println("Your length is " +len);
@@ -25,14 +37,17 @@ public class area
         
         double rectanglearea= len*bre;
         System.out.println("The area of the rectangle is " +rectanglearea);
+        }
         
-        
+        else if(choice==3)
+        {
         System.out.print("Enter the radius of the circle: ");
         double rad = scanner.nextInt();
         System.out.println("Your radius is " +rad);
         
         double circlearea= Math.PI*2*rad;
         System.out.println("The area of the cirle is " +circlearea);
+        }
         
         
         scanner.close();
