bf346561afe21c40c0d49845cfdd7fd6d39d6fd7
Dibyajyoti <dibbispersonal@gmail.com>
2025-01-07 17:40:47 +0530

    Update and rename weirdoperations2.java to weird_operations2.java

--- weird_operations2.java
+++ weird_operations2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class weirdoperations2 {
+public class weird_operations2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
